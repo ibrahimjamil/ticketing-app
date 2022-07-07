601a7a6a35c4299851b41662690316a803f97bf1
ibrahimjamil <ibrahimjamil090@gmail.com>
2022-07-07 14:07:26 +0500

    main: initial commit

---
+++ auth/src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+class Server {
+    private app: express.Application
+    constructor() {
+        this.app = express();
+        this.configuration();
+        this.routes();
+        this.cronScheduler();
+    }
+
+    public configuration() {
+        this.app.use(express.json());
+        this.app.use(morgan('dev'));
+        this.app.set('port', 3000);
+        this.app.use(cors());
+    }
+    public routes(){
+        
+    }
+    public cronScheduler() {
+        
+    }
+    public start(){
+        this.app.listen(this.app.get('port'), () => {
+            console.log(`Server is listening ${this.app.get('port')} port.`);
+        });
+    }
+}
+
+const server = new Server();
+server.start();

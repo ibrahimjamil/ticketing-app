3c5d58fd983279c5d92979ea7749399bc21b82e9
ibrahimjamil <ibrahimjamil090@gmail.com>
2022-07-09 20:07:23 +0500

    add some more generic error handling

--- auth/src/errors/notFoundError.ts
+++ auth/src/errors/notFoundError.ts
@@ -1,12 +1,13 @@
 import { CustomAbstractClass } from "./customAbstractError";
-export class DatabaseError extends CustomAbstractClass {
+
+export class NotFoundError extends CustomAbstractClass {
     public statusCode: number;
     public reason: string;
     constructor(){
-        super('DB error')
-        this.reason = 'DB error';
-        this.statusCode = 500;
-        Object.setPrototypeOf(this, DatabaseError.prototype);
+        super('Route not found')
+        this.reason = 'not found';
+        this.statusCode = 404;
+        Object.setPrototypeOf(this, NotFoundError.prototype);
     }
 
     serializeErrors(){

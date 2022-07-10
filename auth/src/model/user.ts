805427b4a6a40a1f035a4bb2739559984685e16b
ibrahimjamil <ibrahimjamil090@gmail.com>
2022-07-11 03:50:16 +0500

    added some typescript support to intantiate model accordingly

--- auth/src/model/user.ts
+++ auth/src/model/user.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
 
+// interface for user Attribute to pass into
+interface UserAttr {
+    email: string;
+}
+// an interface to describe what user should return 
+interface UserDoc extends mongoose.Document {
+    email: string;
+}
+// an interface to describe what properties userModel should have or to embed into model 
+interface UserModel extends mongoose.Model<UserDoc>{
+    build(attr: UserAttr): UserDoc;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -7,6 +20,11 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema);
+// custom build func to instantiate user according to proper type checking
+userSchema.statics.build = (attr: UserAttr) => {
+    return new User(attr)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
